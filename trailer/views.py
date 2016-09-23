a2eb804c12e12e3f448f6c72bfda52aacb1a3c60
ZHANG Zhipeng <zhipengzhangscut@gmail.com>
2016-09-23 21:24:27 +0800

    [feature] add video_list website

--- trailer/views.py
+++ trailer/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from trailer.models import Video
+from django.shortcuts import render_to_response, HttpResponseRedirect, render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+def video_list_view(requset):
+    video_list = Video.objects.all()
+    video = video_list[0]
+
+    print video.video_name
+    return render_to_response("video_list.html", {"video_list":video_list, "first_video_path":video.video_path,})
+
