57669dcea49a7c136026e3d99dba96c8129c5eef
luotj <simpleltj@gmail.com>
2016-10-14 15:33:22 +0800

    add products

--- trailer/admin.py
+++ trailer/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from trailer.models import Video
+from trailer.models import Video, Product
 
 # Register your models here.
 class VideoAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Video, VideoAdmin)
+admin.site.register(Product)
--- trailer/models.py
+++ trailer/models.py
@@ -17,3 +17,17 @@ class Video(models.Model):
     modified_date = models.DateTimeField(auto_now_add = True)
     def __unicode__(self):
        return str(self.id) + ". " + self.video_name
+
+
+class Product(models.Model):
+   video = models.ForeignKey('trailer.Video', related_name='products')
+   product_name = models.CharField(max_length=50)
+   product_type = models.CharField(max_length=50)
+   product_show_time = models.IntegerField()
+   product_pic_path = models.CharField(max_length=200)
+   product_buy_url = models.CharField(max_length=200)
+   product_key_en = models.CharField(max_length=50)
+   product_key_ch = models.CharField(max_length=50)
+
+   def __str__(self):
+       return str(self.id) + ". " + self.product_name
