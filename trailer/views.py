70587d4daf9b34e24b5373511797f4ede5be0865
ZHANG Zhipeng <zhipengzhangscut@gmail.com>
2016-10-15 22:18:11 +0800

    [feature] show product when it is the product time

--- trailer/views.py
+++ trailer/views.py
@@ -1,4 +1,4 @@
-from trailer.models import Video
+from trailer.models import Video, Product
 from django.shortcuts import render_to_response, HttpResponseRedirect, render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -26,9 +26,19 @@ def video_detail_view(request, video_id):
 @csrf_exempt
 def check_product_info_view(request):
     video_id = request.POST.get('videoId', '')
-    current = request.POST.get('current', '')
+    current_time = request.POST.get('time', '')
+    print(int(float(current_time)))
+    try:
+        video = Video.objects.get(id=video_id)
+        products = Product.objects.filter(video = video, product_show_time = int(float(current_time)))
+        print products
+    except Video.DoesNotExist:
+        video = None
+
+    if products.exists:
+        product = products[0]
     response_data = {}
     if video_id != '':
-        print video_id
         response_data['result'] = 'success'
+        response_data['product_name'] = product.product_name
         return HttpResponse(json.dumps(response_data), content_type="application/json")
